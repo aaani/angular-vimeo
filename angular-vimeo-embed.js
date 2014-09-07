c68bfa56a5ef51d2b77ab65d89bac8d8aa5c154d
Anirvana Mishra <anidps@gmail.com>
2014-09-07 14:21:29 -0400

    Use isolates scope and protocall relative urls

--- angular-vimeo-embed.js
+++ angular-vimeo-embed.js
@@ -2,8 +2,11 @@ angular.module('vimeo', [])
     .directive('vimeoPlayer', ['$http',function ($http) {
         return {
             restrict:'AEC',
+            scope:{
+                videoID:'@videoid'
+            },
             link:function (scope, element) {
-                var id = element.attr('videoid');
+                var id = scope.videoID;
                 if(!id){
                     console.log("angular-vimeo: No videoid attribute set in HTML element");
                     return;
@@ -14,8 +17,11 @@ angular.module('vimeo', [])
                 var vidUrl = oEmbedUrl + '?url=' + encodeURIComponent('http://vimeo.com/'+id);
                 $http({method: 'GET', url: vidUrl}).
                     success(function(data, status, headers, config) {
-                    var vidHTML = '<iframe src="https://player.vimeo.com/video/'+id+'" width="'+w+'" height="'+h+'" frameborder="0" title="'+data.title+'" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+                    var vidHTML = data.html;
                     element[0].innerHTML = unescape(vidHTML);
+                    element[0].children[0].height = h;   
+                    element[0].children[0].width = w; 
+                        
                 }).
                 error(function(data, status, headers, config) {
                     console.log("angular-vimeo: Unable to load video from "+vidUrl);
